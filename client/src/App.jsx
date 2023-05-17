5e1f6d166a36d4ceac95e92341b430f83037847a
“Adilgit config --global user.name “Adil <adilkalbaev2004@gmail.com>
2023-05-17 16:00:10 +0600

    add auth and reg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Page/Home";
 import "../src/App.css";
-
+import Auth from "../src/components/Auth/Auth.jsx";
+import Register from "../src/components/Auth/Register.jsx";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>
