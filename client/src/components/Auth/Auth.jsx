11ea24eaa14a657c889cf7ff48c070578e403e60
“Adilgit config --global user.name “Adil <adilkalbaev2004@gmail.com>
2023-05-18 13:45:20 +0600

    add auth

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,29 +1,86 @@
 import st from "./auth.module.sass";
 import Input from "../../components/ui/Input/Input";
 import Btn from "../ui/Btn/Btn";
+import { useState } from "react";
+import Cookie from "js-cookie";
+import { getUserInfo, loginUser } from "../../api";
+// import { useNavigate } from "react-router-dom";
+// const redirect = useNavigate();
 
 const Auth = () => {
+  const [adminInfo] = useState({
+    token: Cookie.get("key"),
+    isConfirmed: false,
+  });
+
+  const [form, setForm] = useState({
+    identifier: "",
+    password: "",
+  });
+
+  if (adminInfo.token) {
+    getUserInfo(adminInfo.token).then((resp) => {
+      if (resp.role.type === "admin") {
+        adminInfo.isConfirmed = true;
+      }
+    });
+  }
+
+  const handleInput = (event) => {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    loginUser(form)
+      .then((resp) => {
+        Cookie.set("key", resp.jwt);
+        console.log(resp);
+        // redirect("/");
+        window.location.href = "/admin";
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <div className={st.auth__wrap}>
       <div>
-        <div className={st.auth__content}>
-          <div className={st.auth__title}>
-            <h1>Stand Up</h1>
-            <hr />
-            <p>Авторизация</p>
-          </div>
-          <div className={`${st.customInput} ${st.auth__input}`}>
-            <Input idElem="email" />
-            <label htmlFor="email">E-mail</label>
-          </div>
-          <div className={st.auth__btn}>
-            <div className={st.customInput}>
-              <Input idElem="pass" typeElem="password" />
-              <label htmlFor="pass">Пароль</label>
+        <form onSubmit={handleSubmit}>
+          <div className={st.auth__content}>
+            <div className={st.auth__title}>
+              <h1>Stand Up</h1>
+              <hr />
+              <p>Авторизация</p>
             </div>
-            <Btn textBtn="Войти" />
+            <div className={`${st.customInput} ${st.auth__input}`}>
+              <Input
+                value={form.identifier}
+                name="identifier"
+                idElem="email"
+                onchange={handleInput}
+              />
+              <label htmlFor="email">E-mail</label>
+            </div>
+            <div className={st.auth__btn}>
+              <div className={st.customInput}>
+                <Input
+                  onchange={handleInput}
+                  value={form.password}
+                  name="password"
+                  idElem="password"
+                  typeElem="password"
+                />
+                <label htmlFor="pass">Пароль</label>
+              </div>
+              <Btn textBtn="Войти" />
+            </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
--- client/src/components/ui/Input/Input.jsx
+++ client/src/components/ui/Input/Input.jsx
@@ -1,12 +1,14 @@
 import st from "./style.module.sass";
 
-const Input = ({ pHText, dC, idElem, typeElem }) => {
+const Input = ({ pHText, name, onchange, dC, idElem, typeElem }) => {
   return (
     <input
       className={`${st.input} ${dC}`}
       placeholder={pHText}
       id={idElem}
+      name={name}
       type={typeElem}
+      onChange={onchange}
     />
   );
 };
