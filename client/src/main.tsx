25aa594fc858276b74bce776a451328fc76f18a8
NineSirius <ruslanbualah3@gmail.com>
2023-05-19 11:13:45 +0600

    fix ts bugs

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.js'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-)
+const rootElement = document.getElementById('root')
+
+if (rootElement) {
+    ReactDOM.createRoot(rootElement).render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+    )
+}
