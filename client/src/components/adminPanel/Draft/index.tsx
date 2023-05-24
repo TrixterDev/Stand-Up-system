1f1f0205d5766a8ba2ae13c95885ee2924e82aad
k1xd-fr <117137914+k1xd-fr@users.noreply.github.com>
2023-05-24 13:15:43 +0600

    fix

--- client/src/components/adminPanel/Draft/index.tsx
+++ client/src/components/adminPanel/Draft/index.tsx
@@ -1,7 +1,12 @@
-import React from "react";
-
+import st from "./style.module.sass";
 const Draft = () => {
-  return <div>Draft</div>;
+  return (
+    <section className={st.container}>
+      <div className={st.cards}>
+        <h1>sadopfjdsij</h1>
+      </div>
+    </section>
+  );
 };
 
 export default Draft;
