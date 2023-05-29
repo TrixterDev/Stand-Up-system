bb87199064e36738cd3914dfc93434bb4ceb2c84
k1xd-fr <117137914+k1xd-fr@users.noreply.github.com>
2023-05-29 15:17:51 +0600

    fix strapi

--- server/src/api/stand-up/controllers/stand-up.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * stand-up controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::stand-up.stand-up');
--- server/src/api/stand-up/routes/stand-up.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * stand-up router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::stand-up.stand-up');
