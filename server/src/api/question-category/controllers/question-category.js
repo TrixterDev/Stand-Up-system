28e4867095675ba744eec3003f1a33b42263d602
NineSirius <ruslanbulah3@gmail.com>
2023-05-30 11:05:50 +0600

    remove strapi from project

--- server/src/api/question-category/controllers/question-category.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * question-category controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::question-category.question-category');
--- server/src/api/question-category/services/question-category.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * question-category service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::question-category.question-category');
