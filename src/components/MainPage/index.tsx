49c791d702eda638672663f8e7bb3db32b1d310d
Trixter__JDM <54733786+trixtertop@users.noreply.github.com>
2023-05-31 13:04:21 +0600

    fixed avatar user

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -7,7 +7,6 @@ import { Modal } from "../ui/Modal";
 import Card from "./Card";
 import st from "./MainPage.module.sass";
 import Select from "../ui/Select";
-import ava from "../../../public/img/base-avatar.png";
 import { BiExit } from "react-icons/bi";
 import { CgOptions } from "react-icons/cg";
 export interface questionItem {
@@ -72,7 +71,11 @@ const MainPage = () => {
         <button onClick={() => setShowModal(false)}>skip</button>
       </Modal>
       <div className={st.auth}>
-        <Select title={dataUser?.username} src={ava} top={"70px"}>
+        <Select
+          title={dataUser?.username}
+          src="./img/base-avatar.png"
+          top={"70px"}
+        >
           <h4
             style={{
               display: "flex",
